d08e9130c1db6db72d8316a8df33d12e0a71a522
tada <rortheford@gmail.com>
2023-03-02 20:23:28 -0800

    Add function to add/remove classes based on password validity

--- pwdcheck.js
+++ pwdcheck.js
@@ -1,14 +1,36 @@
 function checkPassword() {
     const password = document.querySelector('#user_password');
     const confirm = document.querySelector('#confirm_password');
-    if (password.value !== confirm.value) {
-        password.value = '';
-        confirm.value = '';
-        return false;
-    }
-    else if (password.value !== '' && confirm.value !== '') {
-        myForm.submit();
-        return true;
+    const errormsg = document.querySelector('.errormsg')
+    if (password.value !== "" && confirm.value !== ""){
+        if (password.value !== confirm.value) {
+            password.classList.add("error");
+            confirm.classList.add("error");
+            errormsg.classList.remove("hidden");
+            document.activeElement.blur();
+            password.onfocus = function () {
+                if (password.classList.contains("error")) {
+                    password.value = '';
+                    confirm.value = '';
+                    password.classList.remove("error");
+                    confirm.classList.remove("error");
+                    errormsg.classList.add("hidden");
+                }
+            }
+            confirm.onfocus = function () {
+                if (confirm.classList.contains("error")) {
+                    password.value = '';
+                    confirm.value = '';
+                    password.classList.remove("error");
+                    confirm.classList.remove("error");
+                    errormsg.classList.add("hidden");
+                }
+            }
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
 
